691aef205ac53a8ad416a241a8f54704fd085ee1
wood3 <daijianshan33@outlook.com>
2020-06-02 10:57:05 +0800

    fixed bug

--- 33/HDU/HDU_5616_Jam's balance.cpp
+++ 33/HDU/HDU_5616_Jam's balance.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 int t;
 int N, wi, M, k;
-bool dp[2005];
+bool dp[4005];
 int main()
 {
     ios::sync_with_stdio(false);
@@ -13,20 +13,22 @@ int main()
     {
         cin >> N;
         memset(dp, false, sizeof(dp));
-        dp[0] = 1;
+        dp[0 + 2000] = true;
         for (int i = 1; i <= N; i++)
         {
             cin >> wi;
-            for (int i = 2000; i - wi >= 0; i--)
-                dp[i] = dp[i] || dp[i - wi];
-            for (int i = 0; i + wi <= 2000; i++)
-                dp[i] = dp[i] || dp[i + wi];
+            //wi
+            for (int i = 2000; i - wi >= -2000; i--)
+                dp[i + 2000] = dp[i + 2000] || dp[i + 2000 - wi];
+            //-wi
+            for (int i = -2000; i + wi <= 2000; i++)
+                dp[i + 2000] = dp[i + 2000] || dp[i + 2000 + wi];
         }
         cin >> M;
         for (int i = 1; i <= M; i++)
         {
             cin >> k;
-            if (0 <= k && k <= 2000 && dp[k])
+            if (0 <= k + 2000 && k + 2000 <= 4000 && dp[k + 2000])
                 cout << "YES" << endl;
             else
                 cout << "NO" << endl;
