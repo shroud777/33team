f8f9d334324aaaf891f9f59b80c24d5210d18849
shroud777 <65704849+shroud777@users.noreply.github.com>
2020-08-10 18:20:11 +0800

    Update POJ_3666：Making the Grade：DP，离散化.cpp

--- shroud/POJ/POJ_3666：Making the Grade：DP，离散化.cpp
+++ shroud/POJ/POJ_3666：Making the Grade：DP，离散化.cpp
@@ -6,6 +6,9 @@
 //那么序列前 i-1 个必然已经要做好，且状态转移要从前 i-1 个来
 //那么状态转移方程就是 f[i][j]=min(f[i-1][k],abs(a[i]-v[j]) (1<=k<=j)
 //这一步选哪个数已经决定（即第 i 个位置为 v[j]，所以是 abs(a[i]-v[j])），k 的取值只是限制最后得出的序列是递增的
+//当然，由于每次都沿用 i-1 的结果，还可以用滚动数组优化
+
+//O(n^2) 二维数组
 
 #include <iostream>
 #include <cstdio>
@@ -47,6 +50,49 @@ int main(){
 	}
 	ll ans=1e18;
 	for(int i=0;i<len;i++) ans=min(ans,dp[n][i]);
+	cout<<ans<<endl;
+	return 0;
+}
+
+
+//O(n^2) 滚动数组优化
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector> 
+
+#define pb push_back
+using namespace std;
+typedef long long ll;
+const int maxn=2e3+10;
+vector<int> v;
+int a[maxn];
+ll dp[maxn];
+
+inline int find(int x){
+	return lower_bound(v.begin(),v.end(),x)-v.begin();
+}
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		scanf("%d",&a[i]);
+		v.pb(a[i]);
+	}
+	sort(v.begin(),v.end());
+	v.erase(unique(v.begin(),v.end()),v.end());
+	int len=v.size();
+	for(int i=1;i<=n;i++){
+		ll minn=1e18;
+		for(int j=0;j<len;j++){
+			minn=min(minn,dp[j]);
+			dp[j]=minn+abs(a[i]-v[j]);
+		}
+	}
+	ll ans=1e18;
+	for(int i=0;i<len;i++) ans=min(ans,dp[i]);
 	cout<<ans<<endl;
 	return 0;
 }
