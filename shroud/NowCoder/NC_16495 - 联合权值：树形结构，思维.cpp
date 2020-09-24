39bac504f6fee22dbc83dfd977776873e3caefd6
shroud777 <65704849+shroud777@users.noreply.github.com>
2020-09-24 06:57:24 +0000

    Update NC_16495 - 联合权值：树形结构，思维.cpp

--- shroud/NowCoder/NC_16495 - 联合权值：树形结构，思维.cpp
+++ shroud/NowCoder/NC_16495 - 联合权值：树形结构，思维.cpp
@@ -3,6 +3,56 @@
 //思路：距离为 2 这个条件门槛很低，一开始我想到从根到叶结点的单向性，统计每个节点到他父亲的父亲的联合权值，然后还有每个节点的子节点集间两两构成，还有种更简单的想法
 //后来发现某个序对中间必然经过一个点，那么我们可以枚举中间点，然后对于中间点相邻的所有点，记录它周围点权值的最大值和和以达到 O(n) 计算联合权值之和和最大值
 
+//dfs
+
+#include <bits/stdc++.h>
+
+using namespace std;	
+typedef long long ll;
+const int maxn=2e5+10;
+const int p=1e4+7;
+
+int n;
+int head[maxn],to[maxn<<1],nex[maxn<<1],tot=0;
+ll w[maxn],mx[maxn],sum[maxn],ans=0,Max=0;
+
+inline void add(int u,int v){
+	to[++tot]=v;
+	nex[tot]=head[u];
+	head[u]=tot;
+}
+
+void dfs(int u,int fa){
+	mx[u]=sum[u]=w[fa];
+	for(int i=head[u];i;i=nex[i]){
+		int v=to[i];
+		if(v==fa) continue;
+		Max=max(Max,mx[u]*w[v]);
+		ans=(ans+sum[u]*w[v])%p;
+		sum[u]=(sum[u]+w[v])%p;
+		mx[u]=max(mx[u],w[v]);
+		dfs(v,u);
+	}
+}
+
+int main(){
+	scanf("%d",&n);
+	int u,v;
+	for(int i=1;i<n;i++){
+		scanf("%d %d",&u,&v);
+		add(u,v);
+		add(v,u);
+	}
+	for(int i=1;i<=n;i++) scanf("%lld",&w[i]);
+	dfs(1,0);
+	cout<<Max<<" "<<2*ans%p<<endl;
+	return 0;
+} 
+
+
+
+//中间点方法
+
 #include <bits/stdc++.h>
 
 using namespace std;	
